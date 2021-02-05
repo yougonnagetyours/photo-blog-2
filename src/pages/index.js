8007d84f7bf14a205ac685bbed5ba273568a5a30
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-02-05 11:20:40 +0000

    Add Link to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Home = () => (
+  <section>
+    <h1>Hello world!</h1>
+    <Link to="articles">Articles</Link>
+  </section>
+);
+
+
+export default Home;
