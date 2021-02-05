871e3ae3eed2987fc2dc23337d2f2863f9a3ddfb
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-02-05 14:25:02 +0000

    Add and configure global.scss and gatsby-browser

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"),
+        ],
+      },
+    },
+  ],
+};
